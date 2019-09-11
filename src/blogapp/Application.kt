00bd6d55a6fd809ba61d57b236b883a70f5e861a
Florian Schiller <florian.schiller@schuetze.ag>
2019-09-11 11:11:41 +0200

    initial commit

---
+++ src/blogapp/Application.kt
@@ -0,0 +1,25 @@
+package io.fls.blogapp
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {}
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
